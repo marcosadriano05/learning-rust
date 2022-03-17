8dda343091a07f359c60629dded7ef4a98e960b5
Marcos Adriano <marcosadriano740@gmail.com>
2022-03-17 20:43:03 -0300

    deref trait

---
+++ src/15_2_deref.rs
@@ -0,0 +1,36 @@
+use std::ops::Deref;
+
+fn main() {
+    let x = 5;
+    let y = &x; // reference
+    assert_eq!(5, x);
+    assert_eq!(5, *y); // dereference
+
+    // -----------------
+    let x = 5;
+    let y = Box::new(x); // copy the x value and alocates in Heap memory
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    // -----------------
+    struct MyBox<T>(T);
+
+    impl<T> MyBox<T> {
+        fn new(x: T) -> MyBox<T> {
+            MyBox(x)
+        }
+    }
+
+    impl<T> Deref for MyBox<T> { // implementing Deref trait to allow dereference
+        type Target = T;
+
+        fn deref(&self) ->&Self::Target {
+            &self.0
+        }
+    }
+
+    let x = 5;
+    let y = MyBox::new(x);
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
