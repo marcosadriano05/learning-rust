bb3aab7ac99f422380c67b038483529dba2af6eb
Marcos Adriano <marcosadriano740@gmail.com>
2022-03-17 21:36:25 -0300

    rc smart pointer

---
+++ src/15_4_rc_smat_pointer.rs
@@ -0,0 +1,22 @@
+enum List {
+  Cons(i32, Rc<List>),
+  Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
+// Rc (Reference counted smart pointer) ables that one value take more than one ownership.
+// Theres an count inside the pointer that verify how many data have ownership from a value.
+// This value only be deallocated if all data that have its ownership are droped.
+fn main() {
+  let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+  println!("count after creating a: {}", Rc::strong_count(&a));
+  let _b = Cons(3, Rc::clone(&a));
+  println!("count after creating b: {}", Rc::strong_count(&a));
+  {
+    let _c = Cons(4, Rc::clone(&a));
+    println!("count after creating c: {}", Rc::strong_count(&a));
+  }
+  println!("count after drop c: {}", Rc::strong_count(&a));
+}
