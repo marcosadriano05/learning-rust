bb7966ec8a1a26ff63a797e8da619d96debd61ae
Marcos Adriano <marcosadriano740@gmail.com>
2022-03-17 21:47:22 -0300

    box smart pointer

---
+++ src/15_1_box_smart_pointer.rs
@@ -0,0 +1,12 @@
+enum List {
+  Cons(i32, Box<List>),
+  Nil,
+}
+
+use crate::List::{Cons, Nil};
+
+// Box smart pointer enables store values on heap memory.
+// It permits data structures that compiler don't knows its size can be allocated on heap and referenced to a pointer on stack memory.
+fn main() {
+  let _list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+}
