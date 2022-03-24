3d37b8d5099ec2afeadb7c9c47767b4257312359
Marcos Adriano <marcosadriano740@gmail.com>
2022-03-24 09:39:28 -0300

    apllying format

--- src/17_2_traits_objects.rs
+++ src/17_2_traits_objects.rs
@@ -1,45 +1,42 @@
 // Trait objects are used to represent data that are realationed, implementing polimorphism.
-// To store diferent objects (with diferent types), but implementing the same trait, 
+// To store diferent objects (with diferent types), but implementing the same trait,
 // the dyn keyword is used.
 trait Drawn {
-  fn drawn(&self);
+    fn drawn(&self);
 }
 
 struct Screen {
-  conponents: Vec<Box<dyn Drawn>>,
+    conponents: Vec<Box<dyn Drawn>>,
 }
 
 impl Screen {
-  fn run(&self) {
-    for component in self.conponents.iter() {
-      component.drawn();
+    fn run(&self) {
+        for component in self.conponents.iter() {
+            component.drawn();
+        }
     }
-  }
 }
 
 struct Button {}
 
 impl Drawn for Button {
-  fn drawn(&self) {
-    println!("Inside Button");
-  }
+    fn drawn(&self) {
+        println!("Inside Button");
+    }
 }
 
 struct TextArea {}
 
 impl Drawn for TextArea {
-  fn drawn(&self) {
-    println!("inside TextArea");
-  }
+    fn drawn(&self) {
+        println!("inside TextArea");
+    }
 }
 
 fn main() {
-  let screen = Screen {
-    conponents: vec![
-      Box::new(Button {}),
-      Box::new(TextArea {}),
-    ],
-  };
+    let screen = Screen {
+        conponents: vec![Box::new(Button {}), Box::new(TextArea {})],
+    };
 
-  screen.run();
+    screen.run();
 }
--- src/17_3_state_pattern.rs
+++ src/17_3_state_pattern.rs
@@ -1,44 +1,44 @@
 // Object oriented pattern apllied on Rust: State pattern
 pub struct Post {
-  state: Option<Box<dyn State>>,
-  content: String,
+    state: Option<Box<dyn State>>,
+    content: String,
 }
 
 impl Post {
-  fn new() -> Post {
-    Post {
-      state: Some(Box::new(Draft {})),
-      content: String::new(),
+    fn new() -> Post {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
+        }
     }
-  }
 
-  pub fn add_text(&mut self, text: &str) {
-    self.content.push_str(text);
-  }
-
-  pub fn content(&self) -> &str {
-    self.state.as_ref().unwrap().content(self)
-  }
-
-  pub fn request_review(&mut self) {
-    if let Some(s) = self.state.take() {
-      self.state = Some(s.request_review())
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
     }
-  }
 
-  pub fn approve(&mut self) {
-    if let Some(s) = self.state.take() {
-      self.state = Some(s.approve())
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(self)
     }
-  }
+
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
+        }
+    }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve())
+        }
+    }
 }
 
 trait State {
-  fn request_review(self: Box<Self>) -> Box<dyn State>;
-  fn approve(self: Box<Self>) -> Box<dyn State>;
-  fn content<'a>(&self, post: &'a Post) -> &'a str {
-    ""
-  }
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        ""
+    }
 }
 
 struct Draft {}
@@ -77,19 +77,19 @@ impl State for Published {
     }
 
     fn content<'a>(&self, post: &'a Post) -> &'a str {
-      &post.content
+        &post.content
     }
 }
 
 fn main() {
-  let mut post = Post::new();
+    let mut post = Post::new();
 
-  post.add_text("I ate a salad for lunch today");
-  assert_eq!("", post.content());
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
 
-  post.request_review();
-  assert_eq!("", post.content());
+    post.request_review();
+    assert_eq!("", post.content());
 
-  post.approve();
-  assert_eq!("I ate a salad for lunch today", post.content());
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
